0c567c766d16eca5cce56b96423f09af41d3bdda
barak <baaraak0@gmail.com>
2020-04-11 18:55:36 +0300

    initial commit

---
+++ src/utils/books-client.js
@@ -0,0 +1,11 @@
+import {client} from './api-client'
+
+function search({query = ''}) {
+  return client(`books?query=${encodeURIComponent(query)}`)
+}
+
+function read(bookId) {
+  return client(`books/${bookId}`)
+}
+
+export {search, read}
