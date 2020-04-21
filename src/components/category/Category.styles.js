9f4684ab18cd5d40a533145b913f3582e1f139c6
barak <baaraak0@gmail.com>
2020-04-21 23:14:13 +0300

    fix grid

--- src/components/category/Category.styles.js
+++ src/components/category/Category.styles.js
@@ -7,12 +7,13 @@ export const CategoryContainer = styled.div`
   grid-auto-rows: 0;
   grid-template-rows: auto 1fr;
   overflow: hidden;
+  margin-bottom: 30px;
 `;
 
 export const Title = styled.h4`
   font-size: 25px;
   font-weight: 600;
-  margin: 20px 0;
+  margin: 20px 0 5px;
   display: grid;
   grid-column: 1/-1;
 `;
